239036c5ef0a36e1a72fcb2cd30335ddd5199612
Steven Murr <stevemurr@gmail.com>
2017-11-07 01:18:31 -0800

    reorg

--- main.go
+++ main.go
@@ -16,6 +16,61 @@ import (
 	"github.com/labstack/echo"
 )
 
+var (
+	ffmpegCommand                []string
+	encoderReader, encoderWriter = io.Pipe()
+	streamReader, streamWriter   = io.Pipe()
+	fGetDevices                  = flag.Bool("devices", false, "return the devices")
+	fDeviceName                  = flag.String("name", "", "device name")
+	fBufferSize                  = flag.Int("bufSize", 8192, "size of the port audio buffer")
+	fSampleRate                  = flag.Float64("sr", 48000.0, "samplerate for port audio ")
+	fChannels                    = flag.Int("chans", 2, "number of channels - set incorreclty for interesting effects")
+	fOutputFormat                = flag.String("of", "aac", "output format")
+	// ErrNoDeviceFound --
+	ErrNoDeviceFound = errors.New("no port audio device found with that name")
+	// ErrCouldNotGetDevices --
+	ErrCouldNotGetDevices = errors.New("could not get devices list")
+)
+
+// stream reads the stream from port audio and writes it to the supplied writer
+func stream(w io.Writer, deviceName string, sizeBuf int, sampleRate float64) {
+	d, err := getDeviceByName(deviceName)
+	if err != nil {
+		panic(err)
+	}
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	in := make([]int32, sizeBuf)
+	stream, err := openStream(d, nil, 2, 2, sampleRate, sizeBuf, in)
+	if err != nil {
+		panic(err)
+	}
+	defer stream.Close()
+	if err := stream.Start(); err != nil {
+		panic(err)
+	}
+	for {
+		if err := stream.Read(); err != nil {
+			log.Println(err)
+		}
+		binary.Write(w, binary.LittleEndian, in)
+	}
+}
+
+func streamTranscoder(r io.Reader, w io.Writer, commands []string) {
+	ffmpeg := exec.Command("ffmpeg", commands...)
+
+	ffmpeg.Stdin = r
+	ffmpeg.Stderr = os.Stderr
+	ffmpeg.Stdout = w
+
+	if err := ffmpeg.Run(); err != nil {
+		panic(err)
+	}
+}
+
 // getDevices returns the audio devices on the system
 func getDevices() ([]*portaudio.DeviceInfo, error) {
 	portaudio.Initialize()
@@ -43,29 +98,13 @@ func getDeviceByName(name string) (*portaudio.DeviceInfo, error) {
 
 // openStream will is similar to OpenDefaultStream however it takes a deviceInfo structure as a parameter
 func openStream(inDev *portaudio.DeviceInfo, outDev *portaudio.DeviceInfo, numInputChannels, numOutputChannels int, sampleRate float64, framesPerBuffer int, args ...interface{}) (*portaudio.Stream, error) {
-	p := portaudio.HighLatencyParameters(inDev, outDev)
+	p := portaudio.LowLatencyParameters(inDev, outDev)
 	p.Input.Channels = numInputChannels
 	p.Output.Channels = numOutputChannels
 	p.SampleRate = sampleRate
 	p.FramesPerBuffer = framesPerBuffer
 	return portaudio.OpenStream(p, args...)
 }
-
-var (
-	ffmpegCommand                []string
-	encoderReader, encoderWriter = io.Pipe()
-	streamReader, streamWriter   = io.Pipe()
-	fGetDevices                  = flag.Bool("devices", false, "return the devices")
-	fDeviceName                  = flag.String("name", "", "device name")
-	fBufferSize                  = flag.Int("bufSize", 8192, "size of the port audio buffer")
-	fSampleRate                  = flag.Float64("sr", 48000.0, "samplerate for port audio ")
-	fChannels                    = flag.Int("chans", 2, "number of channels - set incorreclty for interesting effects")
-	fOutputFormat                = flag.String("of", "aac", "output format")
-	// ErrNoDeviceFound --
-	ErrNoDeviceFound = errors.New("no port audio device found with that name")
-	// ErrCouldNotGetDevices --
-	ErrCouldNotGetDevices = errors.New("could not get devices list")
-)
 
 func parseFFMPEGCommand(channels int, outputFormat string, sampleRate float64) []string {
 	sr := ""
@@ -124,43 +163,4 @@ func main() {
 		return c.Stream(http.StatusOK, "audio/aac", streamReader)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// stream reads the stream from port audio and writes it to the supplied writer
-func stream(w io.Writer, deviceName string, sizeBuf int, sampleRate float64) {
-	d, err := getDeviceByName(deviceName)
-	if err != nil {
-		panic(err)
-	}
-
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-
-	in := make([]int32, sizeBuf)
-	stream, err := openStream(d, nil, 2, 2, sampleRate, sizeBuf, in)
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
-	if err := stream.Start(); err != nil {
-		panic(err)
-	}
-	for {
-		if err := stream.Read(); err != nil {
-			log.Println(err)
-		}
-		binary.Write(w, binary.LittleEndian, in)
-	}
-}
-
-func streamTranscoder(r io.Reader, w io.Writer, commands []string) {
-	ffmpeg := exec.Command("ffmpeg", commands...)
-
-	ffmpeg.Stdin = r
-	ffmpeg.Stderr = os.Stderr
-	ffmpeg.Stdout = w
-
-	if err := ffmpeg.Run(); err != nil {
-		panic(err)
-	}
 }
